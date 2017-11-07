b0c1b10e087b47ff48ecac9bffd36aea7cd21f10
VVVa <VVVaSoft@gmail.com>
2017-11-07 20:50:36 +0300

    первый комит

---
+++ CostomList/ApplicationViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace CostomList
+{
+    class ApplicationViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        phonebook selectedPhone;
+        public ObservableCollection<phonebook> Phones { get; set; }
+        public phonebook SelectedPhone  { get{
+            return selectedPhone;}
+           
+         set{
+            selectedPhone=value;
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs("SelectedPhone"));
+        } }
+
+        public ApplicationViewModel() {
+            Phones=phonebook.create();
+
+            removeCommand = new MyCommand(obj =>
+                    {
+                        phonebook phone = obj as phonebook;
+                        if (phone != null)
+                        {
+                            Phones.Remove(phone);
+                        }
+                    },
+                    (obj) => Phones.Count > 0 && selectedPhone!=null);
+
+            addCommand = new MyCommand(obj =>
+                    {
+                        phonebook phone = new phonebook("user","");
+                        
+                        SelectedPhone = phone;
+                        Phones.Insert(0, phone);
+                        PropertyChanged(this, new PropertyChangedEventArgs(""));
+                    });
+        }
+
+        private MyCommand addCommand;
+        public MyCommand AddCommand
+        {
+            get
+            {
+                return addCommand;
+            }
+        }
+        private MyCommand removeCommand;
+        public MyCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand; 
+            }
+        }
+
+    }
+}
---
+++ CostomList/phonebook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace CostomList
+{
+    class phonebook
+    {
+        public string name { get; set; }
+        public string phone { get; set; }
+        public phonebook(string _name, string _phone)
+        { 
+            name=_name;
+            phone=_phone;
+        }
+        public static ObservableCollection<phonebook> create()
+        {
+            ObservableCollection<phonebook> list = new ObservableCollection<phonebook>();
+            list.Add(new phonebook("иванов", "+599808080"));
+            list.Add(new phonebook("петров", "+765855780"));
+            list.Add(new phonebook("сидоров", "+575855780"));
+            return list;
+        }
+    };
+    
+
+}
